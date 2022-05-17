f87f08d90f386d35d03431887b35dd509ad43d13
Umesh Gunasekara <umesh.gunasekara.n@gmail.com>
2022-05-17 11:31:52 +0800

    MORALENOVO - main - UTC202205170330

---
+++ src/main/java/com/slmora/file/organizer/app/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Created by IntelliJ IDEA.
+ * @Author: SLMORA
+ * @DateTime: 7/10/2020 04:45 PM
+ */
+
+package com.slmora.file.organizer.app;
+
+/**
+ * <p>
+ * This Class created for main class for the MoraFileOrganizer001
+ * </p>
+ * @Author: SLMORA
+ * @DateTime: 8/1/2020 11:51 AM
+ * @since 1.0
+ * @implNote bla
+ *
+ * <blockquote><pre><p>
+ * <br>Version      Date            Editor              Note
+ * <br>------------------------------------------------------
+ * <br>1.0          8/1/2020        SLMORA              Initial Code
+ * </p></pre></blockquote><p>
+ *
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+}
